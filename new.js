3ed935c865343c625b20026b868537f540b773e1
unknown <truongchitin2208@gmail.com>
2018-12-18 09:37:33 +0700

    first commit

---
+++ new.js
@@ -0,0 +1,39 @@
+function hideshow() {
+    var div = document.getElementById("js-div");
+    div.classList.toggle("d-none");
+}
+
+function Toggle() {
+    var div = document.getElementById("js-div-1");
+    div.classList.toggle("d-none");
+}
+
+function Toggle1() {
+    var div = document.getElementById("js-div-2");
+    div.classList.toggle("d-none");
+}
+
+function Toggle2() {
+    var div = document.getElementById("js-div-3");
+    div.classList.toggle("d-none");
+}
+
+function Toggle3() {
+    var div = document.getElementById("js-div-4");
+    div.classList.toggle("d-none");
+}
+
+function Toggle4() {
+    var div = document.getElementById("js-div-5");
+    div.classList.toggle("d-none");
+}
+
+function Toggle5() {
+    var div = document.getElementById("js-div-6");
+    div.classList.toggle("d-none");
+}
+
+function Toggle6() {
+    var div = document.getElementById("js-div-7");
+    div.classList.toggle("d-none");
+}
